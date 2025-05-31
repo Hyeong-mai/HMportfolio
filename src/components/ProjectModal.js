7931b81450a90e48b232b39c7a3b1bb89b3f2502
toat1216 <toat1216@hingom.com>
2025-05-31 14:40:09 +0900

    불필요 변수 수정 전 상태 재푸시

--- src/components/ProjectModal.js
+++ src/components/ProjectModal.js
@@ -167,21 +167,6 @@ const TitleWrap = styled.div`
   align-items: center;
 `;
 
-const CoverImageWrap = styled.div`
-  position: absolute;
-  bottom: 0;
-  left: 0;
-  width: 100%;
-  height: 200px;
-  overflow: hidden;
-`;
-
-const CoverImage = styled.img`
-  width: 100%;
-  height: 100%;
-  object-fit: cover;
-`;
-
 const ModalDetail = styled.div`
   padding: 2rem;
   color: white;
